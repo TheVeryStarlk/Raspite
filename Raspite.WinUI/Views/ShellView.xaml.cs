932ad5eb2459f8fe2563267f5b8d93c9d21de6ea
starlkytminecraft@gmail.com <starlkytminecraft@gmail.com>
2022-12-15 20:44:56 +0300

    Add animation to notification

--- Raspite.WinUI/Views/ShellView.xaml.cs
+++ Raspite.WinUI/Views/ShellView.xaml.cs
@@ -1,6 +1,8 @@
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Input;
 using Raspite.WinUI.ViewModels;
+using System.ComponentModel;
 using WinUIEx;
 
 namespace Raspite.WinUI.Views;
@@ -16,6 +18,25 @@ public sealed partial class ShellView : WindowEx
 
         viewModel = App.Current.Services!.GetRequiredService<ShellViewModel>();
         Root.DataContext = viewModel;
+
+        viewModel.PropertyChanged += ViewModelPropertyChanged;
+    }
+
+    private void ViewModelPropertyChanged(object? sender, PropertyChangedEventArgs eventArgs)
+    {
+        if (eventArgs.PropertyName == nameof(viewModel.IsOpen))
+        {
+            if (viewModel.IsOpen)
+            {
+                Notification.Visibility = Visibility.Visible;
+                Notification.Opacity = 1;
+            }
+            else
+            {
+                Notification.Visibility = Visibility.Collapsed;
+                Notification.Opacity = 0;
+            }
+        }
     }
 
     private async void OpenShortcutInvoked(KeyboardAccelerator sender, KeyboardAcceleratorInvokedEventArgs eventArgs)
