657ba09a3621f3f4458c322b99939a2df3aacda3
starlkytminecraft@gmail.com <starlkytminecraft@gmail.com>
2023-04-15 23:05:20 +0300

    Start adding some unit tests.

---
+++ Raspite.Tests/SerializerTests.cs
@@ -0,0 +1,31 @@
+using Raspite.Serializer;
+using Raspite.Serializer.Tags;
+
+namespace Raspite.Tests;
+
+public class SerializerTests
+{
+    [Fact]
+    public async Task SimpleTag_SerializesCorrectly()
+    {
+        var source = new StringTag()
+        {
+            Name = "Raspite",
+            Value = "Serializer"
+        };
+
+        var actual = new byte[]
+        {
+            0x08,
+            0x00, 0x07,
+            0x52, 0x61, 0x73, 0x70, 0x69, 0x74, 0x65,
+            0x00, 0x0A,
+            0x53, 0x65, 0x72, 0x69, 0x61, 0x6c, 0x69, 0x7a, 0x65, 0x72
+        };
+
+        using var expected = new MemoryStream();
+        await BinaryTagSerializer.SerializeAsync(source, expected);
+
+        Assert.Equal(expected.ToArray(), actual);
+    }
+}
