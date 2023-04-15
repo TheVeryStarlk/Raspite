89a11801b599a3d769ec9b242427c86339f6a15e
starlkytminecraft@gmail.com <starlkytminecraft@gmail.com>
2023-04-15 23:30:04 +0300

    Implement all tag but the cursed list tag.

--- Raspite.Serializer/BinaryTagReader.cs
+++ Raspite.Serializer/BinaryTagReader.cs
@@ -31,8 +31,16 @@ internal sealed class BinaryTagReader
         Tag result = type switch
         {
             1 => ReadSignedByteTag(name),
+            2 => await ReadShortTagAsync(name),
+            3 => await ReadIntegerTagAsync(name),
+            4 => await ReadLongTagAsync(name),
+            5 => await ReadFloatTagAsync(name),
+            6 => await ReadDoubleTagAsync(name),
+            7 => await ReadSignedByteCollectionTagAsync(name),
             8 => await ReadStringTagAsync(name),
             10 => await ReadCompoundTagAsync(name),
+            11 => await ReadIntegerCollectionTagAsync(name),
+            12 => await ReadLongCollectionTagAsync(name),
             _ => throw new BinaryTagReaderException("Unknown tag type.")
         };
 
@@ -47,6 +55,73 @@ internal sealed class BinaryTagReader
         {
             Name = name,
             Value = value
+        };
+    }
+
+    private async Task<ShortTag> ReadShortTagAsync(string name)
+    {
+        var value = await stream.ReadShortAsync();
+
+        return new ShortTag()
+        {
+            Name = name,
+            Value = value
+        };
+    }
+
+    private async Task<IntegerTag> ReadIntegerTagAsync(string name)
+    {
+        var value = await stream.ReadIntegerAsync();
+
+        return new IntegerTag()
+        {
+            Name = name,
+            Value = value
+        };
+    }
+
+    private async Task<LongTag> ReadLongTagAsync(string name)
+    {
+        var value = await stream.ReadLongAsync();
+
+        return new LongTag()
+        {
+            Name = name,
+            Value = value
+        };
+    }
+
+    private async Task<FloatTag> ReadFloatTagAsync(string name)
+    {
+        var value = await stream.ReadFloatAsync();
+
+        return new FloatTag()
+        {
+            Name = name,
+            Value = value
+        };
+    }
+
+    private async Task<DoubleTag> ReadDoubleTagAsync(string name)
+    {
+        var value = await stream.ReadDoubleAsync();
+
+        return new DoubleTag()
+        {
+            Name = name,
+            Value = value
+        };
+    }
+
+    private async Task<SignedByteCollectionTag> ReadSignedByteCollectionTagAsync(string name)
+    {
+        var size = await stream.ReadIntegerAsync();
+        var children = await stream.ReadSignedBytesAsync(size);
+
+        return new SignedByteCollectionTag()
+        {
+            Name = name,
+            Children = children
         };
     }
 
@@ -86,5 +161,39 @@ internal sealed class BinaryTagReader
                     break;
             }
         }
+    }
+
+    private async Task<IntegerCollectionTag> ReadIntegerCollectionTagAsync(string name)
+    {
+        var size = await stream.ReadIntegerAsync();
+        var children = new int[size];
+
+        for (var index = 0; index < size; index++)
+        {
+            children[index] = await stream.ReadIntegerAsync();
+        }
+
+        return new IntegerCollectionTag()
+        {
+            Name = name,
+            Children = children
+        };
+    }
+
+    private async Task<LongCollectionTag> ReadLongCollectionTagAsync(string name)
+    {
+        var size = await stream.ReadIntegerAsync();
+        var children = new long[size];
+
+        for (var index = 0; index < size; index++)
+        {
+            children[index] = await stream.ReadLongAsync();
+        }
+
+        return new LongCollectionTag()
+        {
+            Name = name,
+            Children = children
+        };
     }
 }
