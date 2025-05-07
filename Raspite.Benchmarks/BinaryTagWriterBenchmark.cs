cf7935ff0c79f0197cf3a3b29d24b46cf4ebb63b
TheVeryStarlk <starlkytminecraft@gmail.com>
2025-05-08 02:06:18 +0300

    Add benchmarks.

---
+++ Raspite.Benchmarks/BinaryTagWriterBenchmark.cs
@@ -0,0 +1,20 @@
+﻿using System.Buffers;
+using BenchmarkDotNet.Attributes;
+
+namespace Raspite.Benchmarks;
+
+public class BinaryTagWriterBenchmark
+{
+    [Benchmark]
+    public int Simple()
+    {
+        var buffer = new ArrayBufferWriter<byte>(16);
+        var writer = new BinaryTagWriter(buffer, false, false);
+
+        writer.WriteByteTag(byte.MaxValue);
+        writer.WriteShortTag(short.MaxValue);
+        writer.WriteIntegerTag(int.MaxValue);
+
+        return buffer.WrittenSpan.Length;
+    }
+}
--- Raspite.Benchmarks/Program.cs
+++ Raspite.Benchmarks/Program.cs
@@ -1,3 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using BenchmarkDotNet.Running;
 
-Console.WriteLine("Hello, World!");
+var summary = BenchmarkRunner.Run(typeof(Program).Assembly);
