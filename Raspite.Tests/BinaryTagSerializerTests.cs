ca200eda9af628a46b9b42852516f0d86257a80c
TheVeryStarlk <starlkytminecraft@gmail.com>
2025-05-11 03:43:51 +0300

    Write tests.

---
+++ Raspite.Tests/BinaryTagSerializerTests.cs
@@ -0,0 +1,44 @@
+﻿using System.Buffers;
+using Raspite.Tags;
+
+namespace Raspite.Tests;
+
+internal sealed class BinaryTagSerializerTests
+{
+    [Test]
+    public void Serializing_StringTag_IsCorrect()
+    {
+        ReadOnlySpan<byte> correct =
+        [
+            8, 0, 4, 83, 101, 101, 110, 0, 3, 72, 101, 121
+        ];
+
+        var buffer = new ArrayBufferWriter<byte>();
+        var tag = new StringTag("Hey", "Seen");
+
+        BinaryTagSerializer.Serialize(buffer, tag, false);
+
+        Assert.That(buffer.WrittenSpan.ToArray(), Is.EqualTo(correct.ToArray()).AsCollection);
+    }
+
+    [Test]
+    public void Deserializing_StringTag_IsCorrect()
+    {
+        Assert.Multiple(() =>
+        {
+            ReadOnlySpan<byte> correct =
+            [
+                8, 0, 4, 83, 101, 101, 110, 0, 3, 72, 101, 121
+            ];
+
+            var tag = BinaryTagSerializer.Deserialize<Tag>(correct, false);
+
+            Assert.That(tag, Is.AssignableTo<StringTag>());
+
+            var instance = (StringTag) tag;
+
+            Assert.That(instance.Value, Is.EqualTo("Hey"));
+            Assert.That(instance.Name, Is.EqualTo("Seen"));
+        });
+    }
+}
