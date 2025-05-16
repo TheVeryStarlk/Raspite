8370f6964f792abf54c6ef266a02963718f3643c
TheVeryStarlk <starlkytminecraft@gmail.com>
2025-05-17 00:49:50 +0300

    Create end tag.

---
+++ Raspite/Tags/EndTag.cs
@@ -0,0 +1,13 @@
+﻿namespace Raspite.Tags;
+
+public sealed class EndTag : Tag
+{
+    public static EndTag Instance { get; } = new();
+
+    public override byte Identifier => End;
+
+    private EndTag()
+    {
+        // Bad.
+    }
+}
