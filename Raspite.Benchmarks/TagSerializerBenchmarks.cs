dc33ef263200e767efeb124d57b2795c1fbc17be
TheVeryStarlk <starlkytminecraft@gmail.com>
2025-05-19 21:49:37 +0300

    Update benchmarks.

---
+++ Raspite.Benchmarks/TagSerializerBenchmarks.cs
@@ -0,0 +1,47 @@
+﻿using System.Buffers;
+using BenchmarkDotNet.Attributes;
+using Raspite.Tags;
+using Raspite.Tags.Builders;
+
+namespace Raspite.Benchmarks;
+
+public class TagSerializerBenchmarks
+{
+    private readonly CompoundTag tag = CompoundTagBuilder
+        .Create()
+        .AddByteTag(byte.MaxValue, nameof(ByteTag))
+        .AddStringTag("Hello, world!", nameof(StringTag))
+        .AddCompoundTag(CompoundTagBuilder
+            .Create()
+            .AddIntegerTag(int.MaxValue, "Age")
+            .Build())
+        .AddLongsTag([long.MaxValue, long.MaxValue], "Identifiers")
+        .Build();
+
+    private readonly ReadOnlyMemory<byte> source;
+
+    public TagSerializerBenchmarks()
+    {
+        var buffer = new ArrayBufferWriter<byte>();
+
+        TagSerializer.Serialize(buffer, tag);
+
+        source = buffer.WrittenMemory;
+    }
+
+    [Benchmark]
+    public bool Parsing()
+    {
+        return TagSerializer.TryParse(source.Span, out _);
+    }
+
+    [Benchmark]
+    public int Serializing()
+    {
+        var buffer = new ArrayBufferWriter<byte>();
+
+        TagSerializer.Serialize(buffer, tag);
+
+        return buffer.WrittenSpan.Length;
+    }
+}
