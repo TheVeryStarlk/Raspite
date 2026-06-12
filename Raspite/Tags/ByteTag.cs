a2b7fb220da512ba4be51e8119b4d28fbcd7185d
Spi3lot <spi3lot@gmail.com>
2026-06-13 01:06:59 +0200

    #10 Reorder members of ByteTag

--- Raspite/Tags/ByteTag.cs
+++ Raspite/Tags/ByteTag.cs
@@ -2,9 +2,9 @@
 
 public sealed class ByteTag(byte value, string name = "") : Tag<byte>(value, name)
 {
+    public override byte Identifier => Byte;
+    
     public ByteTag(bool value, string name = "") : this((byte) (value ? 1 : 0), name)
     {
     }
-
-    public override byte Identifier => Byte;
 }
