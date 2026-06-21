b3a36573f50d182daef601b8ce2605b5d35c5b49
Spi3lot <spi3lot@gmail.com>
2026-06-21 02:12:45 +0200

    Re-add ListTag's `Get<TTag>` and `GetValue<T>` methods in master branch

--- Raspite/Tags/ListTag.cs
+++ Raspite/Tags/ListTag.cs
@@ -9,4 +9,14 @@ public sealed class ListTag(ImmutableArray<Tag> value, string name = "") : Tag<I
     public Tag this[int index] => Value[index];
 
     public int Length => Value.Length;
+
+    public TTag Get<TTag>(int index) where TTag : Tag
+    {
+        return (TTag) Value[index];
+    }
+
+    public T GetValue<T>(int index)
+    {
+        return ((Tag<T>) Value[index]).Value;
+    }
 }
